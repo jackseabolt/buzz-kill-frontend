1ca7e4cee62500bd36abdd0df689eea278e1972a
Jack Seabolt <jackseabolt@gmail.com>
2017-10-19 14:25:33 -0700

    current jack version

--- src/components/NewPatronForm.test.js
+++ src/components/NewPatronForm.test.js
@@ -6,4 +6,9 @@ describe('<NewPatronForm />', () => {
     it('renders without crashing', () => {
         shallow(<NewPatronForm />); 
     }); 
+    it('displays buttons when formDisplayed is false', () => {
+        const wrapper = shallow(<NewPatronForm />); 
+        expect(wrapper.find('.button_blue')).toEqual(true)
+    }); 
+
 }); 
