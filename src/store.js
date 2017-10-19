785d023d3e4a8431643b872a05300deee7c69b8b
Jack Seabolt <jackseabolt@gmail.com>
2017-10-19 08:19:52 -0700

    more store changes

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'; 
 import {customerReducer} from './reducers'; 
 
-export default createStore(customerReducer, composeWithDevTools(
+export default createStore(customerReducer, 
     applyMiddleware(thunk)
-)); 
+); 
 
