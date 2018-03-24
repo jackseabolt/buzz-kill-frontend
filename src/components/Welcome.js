28b0b5b9045f9d72622af9a3a597d65ba7984d12
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-03-24 16:34:41 -0700

    landing page text altered

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,7 +9,7 @@ class Welcome extends React.Component {
     render() {
         return (
         <div className="welcome">
-            <p className="welcome-p">Welcome! Buzz Kill is application for tracking bar patrons' blood alcohol content. Simply create a record for your patron and 
+            <p className="welcome-p">Welcome! Buzz Kill is application for tracking the blood alcohol content of bar patrons. Simply create a record for your patron and 
                 add drinks as they are served. Buzz Kill takes care of the rest!
             <br />
             </p>
